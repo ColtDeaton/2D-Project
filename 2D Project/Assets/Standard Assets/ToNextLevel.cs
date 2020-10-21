4f13fec371488baad79fa5fa12773b9d654a7dc3
ColtDeaton <videomaster97@gmail.com>
2020-10-21 13:21:17 -0500

    Added a load to next level script

---
+++ 2D Project/Assets/Standard Assets/ToNextLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToNextLevel : MonoBehaviour
+
+{
+    private int LoadLevel;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+        LoadLevel = SceneManager.GetActiveScene().buildIndex + 1;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(LoadLevel);
+    }
+
+
+}
