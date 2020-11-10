ce7d52a4201cf766733265c0fa0e6627b11b2754
Proaustin01 <72824630+Proaustin01@users.noreply.github.com>
2020-11-10 14:00:00 -0600

    Added menu Music, and lives
    I added Menu Music that I got from PartnersInRhyme, and I added a lives system.
    
    The System has some bugs I will need to work out, however for now it works with a few bugs. When you are out of lives it returns you to the main menu.

---
+++ 2D Project/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    Vector2 playerInitPosition;
+
+    void Start()
+    {
+        playerInitPosition = FindObjectOfType<Life>().transform.position;
+    }
+
+    public void Restart()
+    {
+        //saves player's position at start of map will respawn player
+        FindObjectOfType<Life>().transform.position = playerInitPosition;
+    }
+}
---
+++ 2D Project/Assets/Scripts/Life.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Life : MonoBehaviour
+{
+    public GameObject[] lives;
+    private int livesleft;
+    private bool dead;
+
+
+    private void Start()
+    {
+        livesleft = lives.Length;
+    }
+
+    void Update()
+    {
+       if (dead == true)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "damage")
+        {
+            FindObjectOfType<LevelManager>().Restart();
+            if (livesleft >= 1)
+            {
+                livesleft--;
+                Destroy(lives[livesleft].gameObject);
+                if (livesleft < 1)
+                {
+                    dead = true;
+                }
+            }
+            
+        }
+    }
+}
