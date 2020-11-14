35715bc7a13fdbab9c90ba8584390a0816277770
Proaustin01 <72824630+Proaustin01@users.noreply.github.com>
2020-11-13 20:30:24 -0600

    Added Credits, Collectables, and some small bug fixes
    I added 3 collectables which are kept count of in the top right corner, I also added credits and an end screen. Unfortunantly it seems that the animations broke for me.

---
+++ 2D Project/Assets/Scripts/PickUpAndEnd.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class PickUpAndEnd : MonoBehaviour
+{
+    public Text countCol;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        countCol.text = "Collected: " + count.ToString() + "/3";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "collectable")
+        {
+            count += 1;
+            Destroy(other.gameObject);
+            countCol.text = "Collected: " + count.ToString() + "/3";
+        }else if(other.tag == "end")
+        {
+            SceneManager.LoadScene("Credits");
+        }
+    }
+}
